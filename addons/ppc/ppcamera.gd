61f7f58646d6cfc41f80152fdd59cccdbacc51d6
Max Schmitt <max98tonton@gmail.com>
2023-03-15 16:49:53 +0100

    fixed code for godot v.4.0

--- addons/ppc/ppcamera.gd
+++ addons/ppc/ppcamera.gd
@@ -17,28 +17,29 @@ for common 2D top-down strategy games.
 
 Licensed under MIT
 
-v. 0.2.2
+v. 0.4
 
 Author: Max Schmitt 
 		from
 		Divirad - Kepper, Lösing, Schmitt GbR
 """
 
-extends Position2D
-class_name PinchPanCamera, "icon.png"
+@icon("res://res:/addons/ppc/icon.png")
+extends Marker2D
+class_name PinchPanCamera
 
-export var enable_pinch_pan : bool = true
-export (int, "Normal", "Horizontal", "Vertical") var slide_mode = 0
-export var current : bool = true
-export var smoothing : bool = true
-export var smoothing_speed : int = 10
-export var natural_slide : bool = true
+@export var enable_pinch_pan : bool = true
+@export_enum("Normal", "Horizontal", "Vertical") var slide_mode: int 
+@export var enabled : bool = true
+@export var smoothing : bool = true
+@export var position_smoothing_speed : int = 10
+@export var natural_slide : bool = true
 
-export var min_zoom_factor : float = 0.6
-export var max_zoom_factor: float = 2
-export var drag_deadzone : Vector2 = Vector2(0.1, 0.1)
+@export var min_zoom_factor : float = 0.6
+@export var max_zoom_factor: float = 2
+@export var drag_deadzone : Vector2 = Vector2(0.1, 0.1)
 
-export var show_debug_icon : bool = false
+@export var show_debug_icon : bool = false
 
 var shop
 var start_position 
@@ -65,42 +66,42 @@ func _enter_tree():
 	max_zoom = Vector2(max_zoom_factor, max_zoom_factor)
 	
 	var c = load("res://addons/ppc/camera.tscn")
-	add_child(c.instance())
+	add_child(c.instantiate())
 	camera = get_node("camera")
 	
-	camera.drag_margin_left = drag_deadzone.x
-	camera.drag_margin_right = drag_deadzone.x
-	camera.drag_margin_top = drag_deadzone.y
-	camera.drag_margin_bottom = drag_deadzone.y
+	camera.drag_left_margin = drag_deadzone.x
+	camera.drag_right_margin = drag_deadzone.x
+	camera.drag_top_margin = drag_deadzone.y
+	camera.drag_bottom_margin = drag_deadzone.y
 	
-	camera.current = current
-	camera.smoothing_enabled = smoothing
-	camera.smoothing_speed = smoothing_speed
+	camera.enabled = enabled
+	camera.position_smoothing_enabled  = smoothing
+	camera.position_smoothing_speed = position_smoothing_speed
 	
 	if show_debug_icon:
 		var di = load("res://addons/ppc/testicon.tscn")
-		add_child(di.instance())
+		add_child(di.instantiate())
 
 func _process(_delta):
 	
-	if camera.drag_margin_left != drag_deadzone.x \
-	and camera.drag_margin_right != drag_deadzone.x:
-		camera.drag_margin_left = drag_deadzone.x
-		camera.drag_margin_right = drag_deadzone.x
+	if camera.drag_left_margin != drag_deadzone.x \
+	and camera.drag_right_margin != drag_deadzone.x:
+		camera.drag_left_margin = drag_deadzone.x
+		camera.drag_right_margin = drag_deadzone.x
 	
-	if camera.drag_margin_top != drag_deadzone.y \
-	and camera.drag_margin_bottom != drag_deadzone.y:
-		camera.drag_margin_top = drag_deadzone.y
-		camera.drag_margin_bottom = drag_deadzone.y
+	if camera.drag_top_margin != drag_deadzone.y \
+	and camera.drag_bottom_margin != drag_deadzone.y:
+		camera.drag_top_margin = drag_deadzone.y
+		camera.drag_bottom_margin = drag_deadzone.y
 	
-	if camera.current != current:
-		camera.current = current
+	if camera.enabled != enabled:
+		camera.enabled = enabled
 		
-	if smoothing != camera.smoothing_enabled:
-		camera.smoothing_enabled = smoothing
+	if smoothing != camera.position_smoothing_enabled:
+		camera.position_smoothing_enabled = smoothing
 
-	if camera.smoothing_speed != smoothing_speed:
-		camera.smoothing_speed = smoothing_speed
+	if camera.position_smoothing_speed != position_smoothing_speed:
+		camera.position_smoothing_speed = position_smoothing_speed
 
 	if min_zoom != Vector2(min_zoom_factor, min_zoom_factor):
 		min_zoom = Vector2(min_zoom_factor, min_zoom_factor)
@@ -121,11 +122,11 @@ func _input(event):
 		return
 	# Handle MouseWheel for Zoom
 	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == BUTTON_WHEEL_UP:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			emit_signal("zoom_in")
 			if camera.zoom >= min_zoom:
 				camera.zoom -= Vector2(0.1, 0.1)
-		if event.button_index == BUTTON_WHEEL_DOWN:
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			emit_signal("zoom_out")
 			if camera.zoom <= max_zoom:
 				camera.zoom += Vector2(0.1, 0.1)
