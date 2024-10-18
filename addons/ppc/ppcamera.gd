f3ce45c3427143f86ce0233525f25e0d25009d7d
Max Schmitt <max98tonton@gmail.com>
2024-10-18 02:26:30 +0200

    Feature: Inverted zoom, minor changes * added export variables to invert zoom
    Quality of life changes:
    * refactoring
    * export groups
    * Project Settings: added strong typing
    * deleted redundant camera scene
    * renamed "base" camera script
    * strong typing
    * deleted redundant files
    * added new Project Settings for custom *errors*

--- addons/ppc/ppcamera.gd
+++ addons/ppc/ppcamera.gd
@@ -1,88 +1,92 @@
 """
-  _____ _            _       _____            
- |  __ (_)          | |     |  __             
- | |__) | _ __   ___| |__   | |__) |_ _ _ __  
- |  ___/ | '_ | / __| '_ |  |  ___/ _` | '_ | 
+  _____ _            _       _____
+ |  __ (_)          | |     |  __
+ | |__) | _ __   ___| |__   | |__) |_ _ _ __
+ |  ___/ | '_ | / __| '_ |  |  ___/ _` | '_ |
  | |   | | | | | (__| | | | | |  | (_| | | | |
  |_|   |_|_| |_| ___|_| |_| |_|    __,_|_| |_|
-   _____                               
-  / ____|                              
- | |     __ _ _ __ ___   ___ _ __ __ _ 
+   _____
+  / ____|
+ | |     __ _ _ __ ___   ___ _ __ __ _
  | |    / _` | '_ ` _ | / _ | '__/ _` |
  | |___| (_| | | | | | |  __/ | | (_| |
    _____ __,_|_| |_| |_| ___|_|   __,_|
 
-A touchscreen optimized camera control system 
+A touchscreen optimized camera control system
 for common 2D top-down strategy games.
 
 Licensed under MIT
 
 v. 0.4
 
-Author: Max Schmitt 
+Author: Max Schmitt
 		from
 		Divirad - Kepper, Lösing, Schmitt GbR
 """
 
 @icon("res://addons/ppc/icon.png")
-extends Marker2D
-class_name PinchPanCamera
+class_name PinchPanCamera extends Marker2D
 
-@export var enable_pinch_pan : bool = true
-@export_enum("Normal", "Horizontal", "Vertical") var slide_mode: int 
-@export var enabled : bool = true
-@export var smoothing : bool = true
-@export var position_smoothing_speed : int = 10
-@export var natural_slide : bool = true
 
-@export var min_zoom_factor : float = 0.6
+@export var enable_pinch_pan: bool = true
+@export_enum("Normal", "Horizontal", "Vertical") var slide_mode: int
+@export var enabled: bool = true
+@export var natural_slide: bool = true
+
+@export_group("Smoothing")
+@export var smoothing: bool = true
+@export var position_smoothing_speed: int = 10
+
+@export_group("Zoom Options")
+@export var invert_mousewheel_zoom: bool = false
+@export var invert_touch_zoom: bool = false
+@export var min_zoom_factor: float = 0.6
 @export var max_zoom_factor: float = 2
-@export var drag_deadzone : Vector2 = Vector2(0.1, 0.1)
+@export var drag_deadzone: Vector2 = Vector2(0.1, 0.1)
 
-@export var show_debug_icon : bool = false
+@export_group("Debug Options")
+@export var show_debug_icon: bool = false
 
-var shop
-var start_position 
-var already_pressed = false
-var min_zoom : Vector2 = Vector2(0, 0)
-var max_zoom : Vector2 = Vector2(0, 0)
-var naturalizer = 1
+var start_position: Vector2
+var already_pressed: bool = false
+var min_zoom: Vector2 = Vector2(0, 0)
+var max_zoom: Vector2 = Vector2(0, 0)
+var naturalizer: int = 1
 
-var camera : Camera2D
+var camera: TouchZoomCamera2D
 
 signal zoom_in()
 signal zoom_out()
 signal just_pressed()
 signal dragging()
 
-signal input_number(num)
+signal input_number(num: int)
 
-func _enter_tree():
-	"""
-	initializes all the export variables 
-	"""
-	
+## initializes all the export variables
+func _enter_tree() -> void:
 	min_zoom = Vector2(min_zoom_factor, min_zoom_factor)
 	max_zoom = Vector2(max_zoom_factor, max_zoom_factor)
 	
-	var c = load("res://addons/ppc/camera.tscn")
-	add_child(c.instantiate())
-	camera = get_node("camera")
-	
+	var touch_camera := TouchZoomCamera2D.new()
+	touch_camera.name = "TouchZoomCamera2D"
+	add_child(touch_camera)
+	camera = touch_camera
+
 	camera.drag_left_margin = drag_deadzone.x
 	camera.drag_right_margin = drag_deadzone.x
 	camera.drag_top_margin = drag_deadzone.y
 	camera.drag_bottom_margin = drag_deadzone.y
 	
 	camera.enabled = enabled
-	camera.position_smoothing_enabled  = smoothing
+	camera.position_smoothing_enabled = smoothing
 	camera.position_smoothing_speed = position_smoothing_speed
+	camera.invert_zoom = invert_touch_zoom
 	
 	if show_debug_icon:
-		var di = load("res://addons/ppc/testicon.tscn")
+		var di: PackedScene = load("res://addons/ppc/testicon.tscn")
 		add_child(di.instantiate())
 
-func _process(_delta):
+func _process(_delta: float) -> void:
 	
 	if camera.drag_left_margin != drag_deadzone.x \
 	and camera.drag_right_margin != drag_deadzone.x:
@@ -115,54 +119,63 @@ func _process(_delta):
 	elif !natural_slide and naturalizer != -1:
 		naturalizer = -1
 
+	if camera.invert_zoom != invert_touch_zoom:
+		camera.invert_zoom = invert_touch_zoom
 
-func _input(event):
-	
+func _input(event: InputEvent) -> void:
 	if !enable_pinch_pan:
 		return
-	# Handle MouseWheel for Zoom
+
+	## Handle MouseWheel for Zoom
 	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			emit_signal("zoom_in")
-			if camera.zoom >= min_zoom:
+		var invert_modifyer: int = 1
+		if invert_mousewheel_zoom:
+			invert_modifyer = -1
+
+		if invert_mousewheel_zoom:
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP and camera.zoom >= min_zoom:
+				zoom_out.emit()
 				camera.zoom -= Vector2(0.1, 0.1)
-		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			emit_signal("zoom_out")
-			if camera.zoom <= max_zoom:
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and camera.zoom <= max_zoom:
+				zoom_in.emit()
 				camera.zoom += Vector2(0.1, 0.1)
-	
-	# Handle Touch
+		else:
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP and camera.zoom <= max_zoom:
+				zoom_in.emit()
+				camera.zoom += Vector2(0.1, 0.1)
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and camera.zoom >= min_zoom:
+				zoom_out.emit()
+				camera.zoom -= Vector2(0.1, 0.1)
+
+	## Handle Touch
 	if event is InputEventScreenTouch:
 		if event.is_pressed() and !already_pressed:
-			emit_signal("just_pressed")
+			just_pressed.emit()
 			start_position = get_norm_coordinate() * naturalizer
 			already_pressed = true
 		if !event.is_pressed():
 			already_pressed = false
 
-	# Handles ScreenDragging
+	## Handles ScreenDragging
 	if event is InputEventScreenDrag:
 		if camera.input_count == 1:
-			emit_signal("dragging")
+			dragging.emit()
 			if natural_slide:
 				position += get_movement_vector_from(get_local_mouse_position())
 				start_position = get_local_mouse_position()
 			else:
-				var coord = get_movement_vector_from(-get_norm_coordinate())
+				var coord: Vector2 = get_movement_vector_from(-get_norm_coordinate())
 				position += coord
-	# Handles releasing
-	if  camera.input_count == 0:
-		
-		position = camera.get_camera_center() 
-		
 
-func get_movement_vector_from(vec : Vector2) -> Vector2:
-	"""
-	calculates a vector for the movement
-	"""
-	var move_vec = start_position - vec 
-	
-	
+	## Handles releasing
+	if camera.input_count == 0:
+		position = camera.get_camera_center()
+
+
+## calculates a vector for camera movement
+func get_movement_vector_from(vec: Vector2) -> Vector2:
+	var move_vec: Vector2 = start_position - vec
+
 	if slide_mode == 1:
 		return Vector2(move_vec.x, 0)
 	if slide_mode == 2:
@@ -170,19 +183,16 @@ func get_movement_vector_from(vec : Vector2) -> Vector2:
 	else:
 		return move_vec
 
+
+## gets the normalized coordinate of touch
 func get_norm_coordinate() -> Vector2:
-	"""
-	gets the normalized coordinate of a touch
-	"""
-	var result 
+	var result: Vector2 = Vector2.ZERO
 	if natural_slide:
 		result = get_global_mouse_position() - camera.get_camera_center()
 	else:
 		result = get_local_mouse_position() - camera.get_camera_center()
 	return result
-		
-func invert_vector(vec : Vector2):
-	"""
-	inverts a vector
-	"""
+
+
+func invert_vector(vec: Vector2) -> Vector2:
 	return Vector2(-vec.x, -vec.y)
